a2ef30ed5fdef9c4bb2a1912f388fbdae5d7ac3b
Chris <cwill833@gmail.com>
2019-11-16 22:40:49 -0500

    able to send back all quotes

--- Router/quote.js
+++ Router/quote.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const quoteCtrl = require('../controllers/quote')
 
 router.get('/random', quoteCtrl.getRandomQuote)
+router.get('/', quoteCtrl.getAll)
 
 module.exports = router
--- controllers/quote.js
+++ controllers/quote.js
@@ -2,11 +2,22 @@ const { quotes } = require('../data');
 const { getRandomElement } = require('../utils');
 
 module.exports = {
-    getRandomQuote
+    getRandomQuote,
+    getAll
 }
 
 function getRandomQuote(req, res){
     const randoQuote = getRandomElement(quotes)
 
     res.send({quote: randoQuote})
+}
+
+function getAll(req, res){
+    if (Object.keys(req.query).length === 0){
+        console.log("empty")
+        res.send({quotes})
+    }
+
+    
+
 }
